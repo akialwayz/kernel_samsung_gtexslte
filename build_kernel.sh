df55ec80edff2075e07fb71abeb0df0b4953413d
steadfasterX <steadfasterX@gmail.com>
2020-03-19 15:24:35 +0100

    gtexslte: adjusting build script for new dt script
    .. and fixing some other minor stuff
    
    Signed-off-by: steadfasterX <steadfasterX@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,15 +7,17 @@
 
 set -e -o pipefail
 
-export CROSS_COMPILE=~/android-work/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+export CROSS_COMPILE=../../../prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
 # export CROSS_COMPILE=/home/jedld/linaro/toolchain/bin/arm-linux-gnueabihf-
 export ARCH=arm
 
 PLATFORM=sc8830
 DEFCONFIG=gtexslte_defconfig
 
+DTS_FILES=".sprd-scx35l_sharkls_gtexslte_rev00.dtb.dts .sprd-scx35l_sharkls_gtexslte_rev01.dtb.dts .sprd-scx35l_sharkls_gtexslte_rev02.dtb.dts .sprd-scx35l_sharkls_gtexslte_rev03.dtb.dts"
+
 KERNEL_PATH=$(pwd)
-MODULE_PATH=~/android-work/out/target/product/gtexslte/root/lib/modules
+MODULE_PATH=../../../out/target/product/gtexslte/root/lib/modules
 EXTERNAL_MODULE_PATH=${KERNEL_PATH}/external_module
 
 JOBS=`grep processor /proc/cpuinfo | wc -l`
@@ -26,7 +28,7 @@ function build_kernel() {
 	make -j${JOBS}
 	make modules
 	make dtbs
-	./scripts/mkdtimg.sh -i ${KERNEL_PATH}/arch/arm/boot/dts/ -o dt.img
+	./scripts/mkdtimg.sh -o dt.img -ks $KERNEL_PATH -ko $KERNEL_PATH -i $DTS_FILES
 	make -C ${EXTERNAL_MODULE_PATH}/wifi KDIR=${KERNEL_PATH}
 
 	[ -d ${MODULE_PATH} ] && rm -rf ${MODULE_PATH}
