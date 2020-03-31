01cadcd35b2bc02504fb0939420f39f97f6e95b2
steadfasterX <steadfasterX@gmail.com>
2020-03-31 19:12:24 +0200

    gtexslte: build_kernel.sh
    making it even more modular, fixing more issues when building
    in AOSP, allowing to build modules alone, and more ...
    
    Signed-off-by: steadfasterX <steadfasterX@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -16,35 +16,57 @@ DEFCONFIG=gtexslte_defconfig
 
 DTS_FILES=".sprd-scx35l_sharkls_gtexslte_rev00.dtb.dts .sprd-scx35l_sharkls_gtexslte_rev01.dtb.dts .sprd-scx35l_sharkls_gtexslte_rev02.dtb.dts .sprd-scx35l_sharkls_gtexslte_rev03.dtb.dts"
 
+[ ! -z "$KERNEL_PATH" ] && cd $KERNEL_PATH
 KERNEL_PATH=$(pwd)
+
 MODULE_PATH=../../../out/target/product/gtexslte/root/lib/modules
+REC_MODULE_PATH=../../../out/target/product/gtexslte/recovery/root/lib/modules
 EXTERNAL_MODULE_PATH=${KERNEL_PATH}/external_module
 
 JOBS=`grep processor /proc/cpuinfo | wc -l`
 
-function build_kernel() {
-	make ${DEFCONFIG}
-	make headers_install
-	make -j${JOBS}
-	make modules
-	make dtbs
-	./scripts/mkdtimg.sh -o dt.img -ks $KERNEL_PATH -ko $KERNEL_PATH -i $DTS_FILES
-	make -C ${EXTERNAL_MODULE_PATH}/wifi KDIR=${KERNEL_PATH}
+function make_conf() {
+    make -j${JOBS} ${DEFCONFIG}
+}
+
+function build_modules(){
+	make -j${JOBS} headers_install
+	make -j${JOBS} modules
+	make -j${JOBS} -C ${EXTERNAL_MODULE_PATH}/wifi KDIR=${KERNEL_PATH}
 
 	[ -d ${MODULE_PATH} ] && rm -rf ${MODULE_PATH}
 	mkdir -p ${MODULE_PATH}
+	[ -d ${REC_MODULE_PATH} ] && rm -rf ${REC_MODULE_PATH}
+	mkdir -p ${REC_MODULE_PATH}
 
-	find ${KERNEL_PATH}/drivers -name "*.ko" -exec cp -f {} ${MODULE_PATH} \;
-	find -L ${EXTERNAL_MODULE_PATH} -name "*.ko" -exec cp -f {} ${MODULE_PATH} \;
+	#find ${KERNEL_PATH}/drivers -name "*.ko" -exec cp -vf {} ${MODULE_PATH} \;
+	find drivers -name "*.ko" -exec cp -vf {} ${MODULE_PATH} \;
+	find -L ${EXTERNAL_MODULE_PATH} -name "*.ko" -exec cp -vf {} ${MODULE_PATH}  \;
+	#find ${KERNEL_PATH}/drivers -name "*.ko" -exec cp -vf {} ${REC_MODULE_PATH} \;
+	find drivers -name "*.ko" -exec cp -vf {} ${REC_MODULE_PATH} \;
+	find -L ${EXTERNAL_MODULE_PATH} -name "*.ko" -exec cp -vf {} ${REC_MODULE_PATH} \;
+}
+
+function build_kernel() {
+	make -j${JOBS}
+	build_modules
+	make -j${JOBS} dtbs
+	./scripts/mkdtimg.sh -o dt.img -ks $KERNEL_PATH -ko $KERNEL_PATH -i $DTS_FILES
 }
 
 function clean() {
+	echo CLEANING
 	[ -d ${MODULE_PATH} ] && rm -rf ${MODULE_PATH}
-	make distclean
+	make -j${JOBS} distclean
 }
 
 function main() {
-	[ "${1}" = "Clean" ] && clean || build_kernel
+	case "$1" in
+	    clean-modules) clean && make_conf && build_modules ;;
+	    Clean|clean) clean ;;
+	    modules) make_conf && build_modules ;;
+	    *) make_conf && build_kernel ;;
+	esac
 }
 
-main $@
+main "$@"
